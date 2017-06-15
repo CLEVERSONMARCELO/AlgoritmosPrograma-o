f4dc239fca1efed2d0a8c4768d0a2dd3a78ed2af
CLEVERSONMARCELO <cleversom.marcelo@gmail.com>
2017-06-15 12:35:07 -0300

    modificado

--- src/A_P_LISTA_5_Array/Ex18Vetor.java
+++ src/A_P_LISTA_5_Array/Ex18Vetor.java
@@ -1,21 +1,34 @@
 package A_P_LISTA_5_Array;
 
+import java.util.Scanner;
+
 public class Ex18Vetor {
     public static void main(String[] args) {
         
-      int t = Ex9Vetor.leitor("Tamanho do vetor? ");
-      int v = Ex9Vetor.leitor("Número inteiro: ");
-      int[] vetor = Ex9Vetor.cria(t);
-      int [] populaVetor = Ex9Vetor.popula1(vetor);
-        imprime(vetor, v);
-        
+     int tamanhoVetor = Ex9Vetor.leitor("Tamanho do vetor: ");
+     int numero = Ex9Vetor.leitor("número inteiro: ");
+     int [] criaVetor = Ex9Vetor.cria(tamanhoVetor);
+     int [] populaVetor = populaVetor(criaVetor);
+        imprime(populaVetor, numero);
     }
-    static void imprime(int[] vetor, int v) {
+    
+    static int [] populaVetor(int[] vetor) {
+        Scanner leitor = new Scanner(System.in);
+        
         for (int i = 0; i < vetor.length; i++) {
+                System.out.printf("Posição %d do vetor recebe: ", (i+1));
+                vetor[i] = leitor.nextInt();
+        }
+        return vetor;
+    }
 
-            if (vetor[i] == v) {
-                System.out.printf("Posição %d do vetor contém: %d  ", (i+1), vetor[i]);
-                System.err.print("<-------Número inteiro está nesta posição");
+    static void imprime (int [] vetorPopulado, int numero) {
+
+        for (int i = 0; i < vetorPopulado.length; i++) {
+
+            if (vetorPopulado[i] == numero) {
+                System.out.println("Posição "+(i+1)+" do vetor contém: "+ vetorPopulado[i] + 
+                        "    <------- número inteiro está nesta posição ");
             }
         }
     }
